95a3d82462a6d0d4cc8105a3c920efd61caf5b30
Manosh Malai <33615484+mmalai@users.noreply.github.com>
2020-03-27 23:29:15 +0530

    replaced old shellcheck hook script

--- hooks/shellcheck.sh
+++ hooks/shellcheck.sh
@@ -1,16 +1,14 @@
 #!/usr/bin/env bash
 # Original: https://github.com/jumanjihouse/pre-commit-hooks#shellcheck
 # Forked to change runtime to /usr/bin/env on win10
+set -eu
+
+# Ensure shell scripts conform to shellcheck.
+# https://www.shellcheck.net/
 
 readonly DEBUG=${DEBUG:-unset}
 if [ "${DEBUG}" != unset ]; then
   set -x
 fi
 
-bin="$(command -v shellcheck)"
-
-while IFS= read -r -d '' file
-do
-  (( count++ ))
-  "${bin}" "$@" "$file"
-done <   <(find . -type f -name "*.sh" -print0)
+shellcheck "$@"
